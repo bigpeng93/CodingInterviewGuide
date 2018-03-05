1b59b121cafff4e700a82b06d40ca3e4a46f7943
zhangdapeng <1099469985@qq.com>
2018-03-05 22:25:51 +0800

    LinkedQuestion

---
+++ src/BinaryTree/Morris/Solution.java
@@ -0,0 +1,10 @@
+package BinaryTree.Morris;
+
+/**
+ * 遍历二叉树的神级方法
+ *
+ * 给定一颗二叉树的头节点head，完成二叉树的先序、中序和后序遍历。
+ * 如果二叉树的节点数为N，要求时间复杂度为O(N),额外空间复杂度为O(1)。
+ */
+public class Solution {
+}
---
+++ src/BinaryTree/PrintTree/Solution.java
@@ -0,0 +1,18 @@
+package BinaryTree.PrintTree;
+
+import BinaryTree.Node.Node;
+
+/**
+ * 如何比较直观地打印二叉树
+ *
+ * 二叉树可以用常规的三种遍历结果来描述其结构，但是不够直观，尤其是二叉树中有重复值的时候，
+ * 仅通过三种遍历的结果来构造二叉树的真实结构更是难上加难，有时则根本不可能。
+ * 给定一棵二叉树的头节点head，已知二叉树节点值的类型为32位整数，请实现一个打印二叉树的函数，
+ * 可以直观地展示树的形状，也便于画出真实的结构。
+ */
+
+public class Solution {
+    public void printTree(Node head){
+
+    }
+}
