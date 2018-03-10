d1e7b913e1070dad18acb94a74c4ec4d3a75deaa
zhangdapeng <1099469985@qq.com>
2018-03-10 20:19:28 +0800

    RecursionAndDynamic

--- src/RecursionAndDynamic/MinCoins/ExtQue.java
+++ src/RecursionAndDynamic/MinCoins/ExtQue.java
@@ -15,4 +15,56 @@ package RecursionAndDynamic.MinCoins;
  *  不用任何货币就可以组成0元，返回0.
  */
 public class ExtQue {
+    public int minCoins(int[] arr,int aim){
+        if (aim < 0|| arr.length<=0||arr==null)
+            return -1;
+        int n = arr.length;
+        int[][] dp = new int[n][aim+1];
+        int max= Integer.MAX_VALUE;
+        int left = 0;
+        for (int i = 1;i<=aim;i++){
+            dp[0][i] = max;
+        }
+        if (arr[0] <=aim){
+            dp[0][arr[0]] = 1;
+        }
+        for (int i =1;i<n;i++){
+            for (int j=1;j<=aim;j++){
+                left = max;
+                if (j-arr[i]>=0 && dp[i-1][j-arr[i]] !=max)
+                    left = dp[i-1][j-arr[i]]+1;
+                dp[i][j] = Math.min(left,dp[i-1][j]);
+            }
+        }
+        return dp[n-1][aim]!=max ? dp[n-1][aim]:-1;
+
+    }
+
+    public int minCoinsTwo(int[] arr,int aim){
+        if (aim == 0|| arr.length<=0||arr==null)
+            return 0;
+        int n = arr.length;
+        int[] dp = new int[aim+1];
+        int max= Integer.MAX_VALUE;
+        int left = 0;
+        for (int i = 1;i<=aim;i++){
+            dp[i] = max;
+        }
+        if (arr[0] <=aim){
+            dp[arr[0]] = 1;
+        }
+        for (int i =1;i<n;i++){
+            //需要反向遍历，否则会出现重复使用。
+            for (int j = aim;j>0;j--){
+                left = max;
+                if (j-arr[i]>=0 && dp[j-arr[i]]!=max){
+                    left = dp[j-arr[i]] + 1;
+                }
+                dp[j] = Math.min(left,dp[j]);
+            }
+
+        }
+        return dp[aim]!=max ? dp[aim]:-1;
+
+    }
 }
--- src/RecursionAndDynamic/MinCoins/OrgQuestion.java
+++ src/RecursionAndDynamic/MinCoins/OrgQuestion.java
@@ -3,7 +3,7 @@ package RecursionAndDynamic.MinCoins;
 /**
  * 换钱的最少货币数
  *
- * 给定数组arr，arr中所有的值都为正数且不重复。每个值代表一种面值的货币，每种面值的额货币可以使用任意张，
+ * 给定数组arr，arr中所有的值都为正数且不重复。每个值代表一种面值的货币，每种面值的货币可以使用任意张，
  * 再给定一个整数aim代表要找的钱数，求组成aim的最少货币数。
  *
  * 举例：
@@ -16,4 +16,55 @@ package RecursionAndDynamic.MinCoins;
  *
  */
 public class OrgQuestion {
+    public int minCoins(int[] arr,int aim){
+        if (arr==null ||aim<=0||arr.length<=0)
+            return 0;
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[][] dp = new int[n][aim+1];
+        //第一列：表示找的钱数为0时需要的最少张数，即全为0.初始化时，全为0.
+        //第一行：表示只能使用arr[0]货币的情况下，找某个钱数的最小张数。
+        for(int i = 1;i<=aim;i++){
+            dp[0][i] = max;
+            if(i-arr[0] >=0 && dp[0][i-arr[0]] !=max){
+                dp[0][i] = dp[0][i-arr[0]]+1;
+            }
+        }
+        int left = 0;
+        for (int i =1;i<n;i++){
+            for (int j =1;j<=aim;j++){
+                left = max;
+                if(j-arr[i] >=0 && dp[i][j-arr[i]] !=max){
+                    left = dp[i][j-arr[i]]+1;
+                }
+                dp[i][j] = Math.min(left,dp[i-1][j]);
+            }
+        }
+        return dp[n-1][aim] !=max ?dp[n-1][aim]:-1;
+    }
+
+    public int minCoinsTwo(int[] arr,int aim){
+        if (arr==null ||aim<=0||arr.length<=0)
+            return 0;
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[] dp = new int[aim+1];
+        for (int i = 1;i<=aim;i++){
+            dp[i] = max;
+            if(i-arr[0] >=0 && dp[i-arr[0]] !=max){
+                dp[i] = dp[i-arr[0]]+1;
+            }
+        }
+        int left = 0;
+        for (int i =1;i<n;i++){
+            for (int j =1;j<=aim;j++){
+                left = max;
+                if(j-arr[i] >=0 && dp[j-arr[i]] !=max){
+                    left = dp[j-arr[i]]+1;
+                }
+                dp[j] = Math.min(left,dp[j]);
+            }
+        }
+        return dp[aim] !=max ?dp[aim]:-1;
+    }
 }
